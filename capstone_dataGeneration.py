a796be8bdc680247e56611afef4069bb09ccc640
jhelland <jhelland@mines.edu>
2017-04-03 12:26:29 -0600

    Add files via upload

--- capstone_dataGeneration.py
+++ capstone_dataGeneration.py
@@ -206,33 +206,24 @@ mydata[14, 2:6] = np.exp(mydata[14, 2:6])
 
 #e^k
 
+boot_kA = get_bootstrap(normData1[:, :5].T, lambda x: get_k(times[3:8], x), n)
+boot_kB = get_bootstrap(normData1[:, 5:].T, lambda x: get_k(times[3:8], x), n)
 
-def e_k(data):
-    d = data
-    dA = np.exp(-apply(d[:, 0:5], lambda x: get_k(times[3:8], x), 0))
-    dB = np.exp(-apply(d[:, 5:10], lambda x: get_k(times[3:8], x), 0))
-    return [dA, dB]
-
-
-boot_k = get_bootstrap(normData1, e_k, n)
+boot_k = [np.concatenate([boot_kA[0], boot_kB[0]]),
+          np.concatenate([boot_kA[1], boot_kB[1]], axis=1)]
 
 mydata[15:20, 2:6] = get_cis(boot_k)
 mydata[19, 2:6] = np.exp(mydata[19, 2:6])
-
 
 ###############
 
 # C
 
+boot_CA = get_bootstrap(normData1[:, :5].T, lambda x: get_C(times[3:8], x), n)
+boot_CB = get_bootstrap(normData1[:, 5:].T, lambda x: get_C(times[3:8], x), n)
 
-def C(data):
-    d = data
-    dA = np.exp(apply(d[:, 0:5], lambda x: get_C(times[3:8], x), 0))
-    dB = np.exp(apply(d[:, 5:10], lambda x: get_C(times[3:8], x), 0))
-    return [dA, dB]
-
-
-boot_C = get_bootstrap(normData1, C, n)
+boot_C = [np.concatenate([boot_CA[0], boot_CB[0]]),
+          np.concatenate([boot_CA[1], boot_CB[1]], axis=1)]
 
 mydata[20:25, 2:6] = get_cis(boot_C)
 mydata[24, 2:6] = np.exp(mydata[24, 2:6])
